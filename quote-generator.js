a898d07cbafa3565287ef8ac28e76bbc36995898
Brandon <bparkerproductions@gmail.com>
2016-02-22 11:18:26 -0900

    quote generates on button click

--- quote-generator.js
+++ quote-generator.js
@@ -1,13 +1,15 @@
 var Data = {
-    url: "http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?",
-
-    fake_url: "f"
+    url: "http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?"
 }
 
 var App = {
     init: function(){
-        View.generate_quote();
+        App.set_event_listeners();
     },
+
+    set_event_listeners: function(){
+        $("#quote-selector button").on("click", View.generate_quote)
+    }
 
 }
 
