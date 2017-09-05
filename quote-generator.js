f10198d829df15e4cb4c3ef6309b8ebdce93f66c
Brandon <bparkerproductions@gmail.com>
2017-09-04 21:56:52 -0800

    data cleaning and helper functions

--- quote-generator.js
+++ quote-generator.js
@@ -22,18 +22,26 @@ var View = {
             }
             else{
                 $("#author b").html("~ Anonymous");
+                View.clearFeed();
             }
         });
     },
 
-    generateFeed: (title, url) => {
-        var entryStr = `
-            <li>
-                <a href="${url}">${title}</a>
-            </li>
-        `;
-        
-        $("#redditFeed").append(entryStr);
+    generateFeed: (title, url, term) => {
+
+        if(redditWrap.filterTitle(title, term)){
+            var entryStr = `
+                <li>
+                    <a href="${url}">${title}</a>
+                </li>
+            `;
+
+            $("#redditFeed").append(entryStr);
+        }
+    },
+
+    clearFeed: () => {
+        $("#redditFeed").empty();
     }
 }
 
@@ -42,13 +50,23 @@ var redditWrap = {
 
     searchByAuthor: (author) => {
         var title = author.trim().split(" ").join("+");
-        var searchURL = `https://www.reddit.com/r/quotes/search.json?q=${title}&sort=popular&limit=100`;
+        var searchURL = `https://www.reddit.com/r/quotes/search.json?q=${title}&sort=popular&limit=30`;
 
         var data = redditWrap.grabJSON(searchURL);
+        View.clearFeed(); //clear feed for regenerating
         data.then((obj)=>{
-            obj.data.children.forEach((e)=>{
-                View.generateFeed(e.data.title, e.data.url)
+            var filtered = redditWrap.removeDups(obj.data.children);
+            console.log(filtered)
+            filtered.forEach((e)=>{
+                View.generateFeed(e.data.title, e.data.url, author);
             })
+        })
+    },
+
+    removeDups: (arr) => {
+        return arr.filter((elem, i)=>{
+            console.log(elem)
+            return arr.indexOf(elem) === i;
         })
     },
 
@@ -56,5 +74,9 @@ var redditWrap = {
         return new Promise((resolve, reject)=>{
             $.getJSON(url, (data) => {resolve(data)});
         })
-    }
+    },
+
+    filterTitle: (title, searchTerm) =>{
+        return title.includes(searchTerm) ? true : false;
+    },
 }
